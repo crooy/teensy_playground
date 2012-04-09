1e81cc955de3c3943b5ebee56d68eadf262e572e
Ronald Crooy <r.a.crooy@gmail.com>
2012-04-09 20:21:33 +0200

    yay, temperature counter

--- arduino/thermometer/sketch_apr09a/sketch_apr09a.ino
+++ arduino/thermometer/sketch_apr09a/sketch_apr09a.ino
@@ -1,12 +1,13 @@
-const int redPin =  12;
-const int greenPin =  15;
-const int bluePin =  14;
+const int redPin =  15;
+const int greenPin =  14;
+const int bluePin =  12;
 const int buttonPin = 7;
 const int wheelPin = 0;
 const int statusLed = 11;
 const int tempSensor = 1;
 
   void setup()   {
+
     pinMode(statusLed, OUTPUT);
     pinMode(redPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
@@ -15,9 +16,10 @@ const int tempSensor = 1;
     pinMode(buttonPin, INPUT_PULLUP);
     
     attachInterrupt(2, onButtonPress, FALLING);
+
   }
   const int colorIndex[3] = {redPin, greenPin, bluePin};
-  volatile int colorValue[3] = {0, 0, 0};
+  volatile int colorValue[3] = {0, 0, 255};
   volatile int selectedColor = 0;
   // 0 : red
   // 1 : blue
@@ -27,12 +29,34 @@ const int tempSensor = 1;
   // 0 : idle
   // 1 : settings
   
+  boolean goingToRed = true;
+  
   void loop()
   {
     if (state == 1)
     {
         blinkTemperature(getTemperature());
         state = 0;  
+    }else{
+      
+      if (goingToRed)
+      {
+        colorValue[0]+=1;
+        colorValue[2]-=1;
+      }else{
+        colorValue[0]-=1;
+        colorValue[2]+=1;
+      }
+      if (colorValue[0]==255)
+      {
+          goingToRed = false;
+      }
+      if (colorValue[2]==255)
+      {
+          goingToRed = true;
+      }            
+      setColor();
+      delay(50);
     }
   }
 
@@ -60,6 +84,14 @@ const int tempSensor = 1;
     //converting from 10 mv
     //per degree wit 500 mV  
     //degrees ((volatge - 500mV) times 100)
+  }
+  
+  void setColor()
+  {
+        Serial.println("color : "+String(colorValue[0])+","+String(colorValue[1])+","+String(colorValue[2]));
+    analogWrite(colorIndex[0], colorValue[0]);
+    analogWrite(colorIndex[1], colorValue[1]);
+    analogWrite(colorIndex[2], colorValue[2]);
   }
   
   void blinkTemperature(float temp)
