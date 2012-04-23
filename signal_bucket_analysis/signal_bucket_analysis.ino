988755a889ee6d46dd4799f4236e11a6d3a17e9d
Ronald Crooy <r.a.crooy@gmail.com>
2012-04-23 07:25:57 +0200

    small changes

--- signal_bucket_analysis/signal_bucket_analysis.ino
+++ signal_bucket_analysis/signal_bucket_analysis.ino
@@ -3,20 +3,20 @@ const int txpin = 8;
 const int rxpin = 7;
 const int buttonPin = 6;
 const int analogPin = 0;
-const unsigned long MAXT = 10000;
+const unsigned long MAXT = 200;
 const int BUCKET_SIZE = 2;
-const int BUCKETS = 1000;
+const int BUCKETS = (MAXT/BUCKET_SIZE)+1;
 
 void setup()
 {
   Serial.begin(38400) ;
   Serial.println("setup\n\n");
-  
+
   pinMode(ledpin, OUTPUT);
   pinMode(rxpin, INPUT);
   pinMode(txpin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
-  
+
   attachInterrupt(1, buttonPressed, FALLING);
   attachInterrupt(2, signalChange, CHANGE);
 
@@ -26,7 +26,7 @@ void setup()
 
 volatile unsigned long dt=0;
 volatile unsigned long markedTime=0;
-volatile unsigned int bucket[BUCKETS-1];
+volatile unsigned long bucket[BUCKETS-1];
 
 void initBuckets()
 {
@@ -39,26 +39,28 @@ void initBuckets()
 void buttonPressed()
 {
   noInterrupts();
-  digitalWrite(ledpin, HIGH);
   printBucket();
-  delay(2000);
-  digitalWrite(ledpin, LOW);
   interrupts();
 }
 
 void printBucket()
 {
   unsigned long sum = 0;
-  for(int i=0; i< BUCKETS; ++i)
+  for(int i=0; i< BUCKETS-1; ++i)
   {
     sum += bucket[i];
   }
   unsigned long minimum = sum / 100;
   Serial.println();
-  for(int i=0; i< BUCKETS; ++i)
+  for(int i=0; i< BUCKETS-1; ++i)
   {
     if (bucket[i] > minimum ){
-      Serial.print(i*BUCKET_SIZE);
+      int bucketrange = i*BUCKET_SIZE;
+      Serial.print("[");
+      Serial.print(bucketrange-BUCKET_SIZE);
+      Serial.print("-");
+      Serial.print(bucketrange);
+      Serial.print("] microsec");
       Serial.print(" : ");
       Serial.println(bucket[i]);
     }
@@ -81,7 +83,11 @@ void signalChange()
 
 void loop()
 {
-
+  digitalWrite(ledpin, HIGH);
+  delay(2000);
+  digitalWrite(ledpin, LOW);
+  delay(2000);
 }
+
 
 
